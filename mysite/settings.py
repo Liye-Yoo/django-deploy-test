01e677d4eeddc8ef461cfdfabad63ca9058cce86
Liye_Yoo <lyyoo38@gmail.com>
2021-03-11 02:02:48 +0900

    allowed host update

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = 'c5q93gewvlh5idzuv4*-gl+)plfsp$^i-a!*^56!+5%rc+-cqk'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    ".ap-northeast-2.compute.amazonaws.com"
+]
 
 
 # Application definition
